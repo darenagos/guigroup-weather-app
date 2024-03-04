3fded09aa72faa1bb3f1de53a30fc0298963ee8f
darenagos <darenagos@gmail.com>
2024-03-04 13:29:51 +0000

    base code

---
+++ gui-weather-app/src/App.js
@@ -0,0 +1,29 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>Weather Forecast App</h1>
+        <Weather />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
