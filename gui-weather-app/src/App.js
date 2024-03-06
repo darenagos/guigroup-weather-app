d82af9956ba429d0f471f7477e765e8432737e8d
Rucha2284 <ruchamgothoskar@gmail.com>
2024-03-06 11:46:02 +0000

    css-1

--- gui-weather-app/src/App.js
+++ gui-weather-app/src/App.js
@@ -5,25 +5,33 @@ import Weather from "./Weather";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Weather Forecast App</h1>
-        <Weather />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="base">
+
+      <div class="main-photo">
+        <div class="search-bar">
+          <div class="search-logo"></div>
+          <p class="search-text">search</p>
+          <div class="line-img"></div>  {/*NEED HELP WITH THIS */}
+        </div>
+      </div>
+
+      <div class="weather-table"></div>
+
+      <div class="tip"></div>
+
+      <div class="caution">
+        <div class="heading-caution">
+          <div class="caution-logo1"></div>
+          <p class="caution-heading-text">Caution</p>
+          <div class="caution-logo2"></div>
+        </div>
+      </div>
+
+      <button type="button" class="tip-button">Tips</button>
+
+      <div class="map-position">MAP API</div>
     </div>
-  );
+  )
 }
 
 export default App;
