4a49beebd477bd9d90aae3d365bb463b64ac5947
darenagos <darenagos@gmail.com>
2024-03-20 17:07:20 +0000

    adding box shadow to image to indicate time

--- gui-weather-app/src/Components/CautionTipsSection/CautionTipsSection.js
+++ gui-weather-app/src/Components/CautionTipsSection/CautionTipsSection.js
@@ -1,23 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../App.css";
 import Popup from "../Popup-open";
 
 function Caution() {
-  // const[popup, setPop] =useState(false);
-  // const handleClick=()=>{
-  //   setPop(!popup);
-  // }
   const [buttonPopup, setButtonPopup] = useState(false);
+  const [tipImageStyle, setTipImageStyle] = useState({});
+
+  useEffect(() => {
+    const currentTime = new Date().getHours();
+
+    // Define your thresholds for changing colors based on time of day
+    const pinkThresholdStart = 12; // 12 PM
+    const pinkThresholdEnd = 17; // 5 PM
+    const blueThreshold = 17; // 5 PM
+
+    // Calculate color based on current time
+    let boxShadowColor;
+    if (currentTime < pinkThresholdStart) {
+      // Before 12 PM, set orange box shadow
+      boxShadowColor = "rgba(255, 125, 0, 0.8)";
+    } else if (
+      currentTime >= pinkThresholdStart &&
+      currentTime < pinkThresholdEnd
+    ) {
+      // Between 12 PM and 5 PM, set pink box shadow
+      boxShadowColor = "rgba(255, 192, 203, 0.9)";
+    } else {
+      // After 5 PM, set blue box shadow
+      boxShadowColor = "rgba(0, 51, 108, 0.8)";
+    }
+
+    console.log("Current Time:", currentTime);
+    console.log("Box Shadow Color:", boxShadowColor);
+
+    // Set the style for the tip image box shadow
+    setTipImageStyle({ boxShadow: `0px 0px 40px ${boxShadowColor}` });
+  }, []);
+
   return (
     <div>
-      {/* {popup ?
-      <div className="Popup">
-      HEY
-    </div>
-      :""} */}
-      
       <div className="grid-container-imgTipsCaution">
-        <div className="tip-image grid-row-span-2"></div>
+        <div className="tip-image grid-row-span-2" style={tipImageStyle}></div>
         <div className="caution">
           <div>
             <div className="heading-caution">
@@ -36,7 +59,11 @@ function Caution() {
           </div>
         </div>
         <div>
-          <button type="button" className="tip-button" onClick={() => setButtonPopup(true)}>
+          <button
+            type="button"
+            className="tip-button"
+            onClick={() => setButtonPopup(true)}
+          >
             Tips
           </button>
         </div>
